ada96d4004231b1e88af29e91f33bdf7294eab25
Bruce Doan <rgv151@gmail.com>
2017-07-03 10:18:33 +0700

    replaced ``%`` operator w/ `toHex` proc, closes #1

--- hmac.nim
+++ hmac.nim
@@ -31,7 +31,7 @@ iterator items(s: SecureHash): uint8 =
   for n in Sha1Digest(s):
     yield n
 
-proc `%`*[T](x: T): string =
+proc toHex*[T](x: T): string {.inline.} =
   when x is SecureHash:
     toLower($x)
   elif x is MD5Digest:
@@ -78,27 +78,27 @@ proc hmac_md5*(key, data: string): MD5Digest =
 
 when isMainModule:
 
-  var result = %hmac_sha1("key", "The quick brown fox jumps over the lazy dog")
+  var result = toHex(hmac_sha1("key", "The quick brown fox jumps over the lazy dog"))
   echo result
   assert(result == "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9","Incorrect hash")
 
   let longKey = "oiJkCotyEAcqEtbHAxwR0sj7Fl4CAT2xdT2oYJep6Wzes2umipBUzocVSwp7nL5ns4xDrPIBEBHKwIr3LlQLZmCw1wStOMSke9SDvQ2Gayj5ZGzvZ1T1uVyN4DcGenYd"
-  result = %hmac_sha1(longKey, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut nibh sit amet felis volutpat pellentesque eu at tellus. Etiam posuere justo eget mi porta porta.")
+  result = toHex(hmac_sha1(longKey, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut nibh sit amet felis volutpat pellentesque eu at tellus. Etiam posuere justo eget mi porta porta."))
   echo result
   assert(result == "a0d87be75bb531af746ab7988e1e8058e7bc17f0","Incorrect hash")
 
-  result = %hmac_sha256("ubuntu", "Canonical to offer 5 years of support, but Snap packages mean latest features factor in.")
+  result = toHex(hmac_sha256("ubuntu", "Canonical to offer 5 years of support, but Snap packages mean latest features factor in."))
   echo result
   assert(result == "f53abed8001d0b7c8a64edc011854bded49e1ed55e5d5f5455b7b2ecf6506884", "Incorrect hash")
 
-  result = %hmac_sha256(longKey, "Nim (formerly known as \"Nimrod\") is a statically typed, imperative programming language that tries to give the programmer ultimate power without compromises on runtime efficiency. This means it focuses on compile-time mechanisms in all their various forms.")
+  result = toHex(hmac_sha256(longKey, "Nim (formerly known as \"Nimrod\") is a statically typed, imperative programming language that tries to give the programmer ultimate power without compromises on runtime efficiency. This means it focuses on compile-time mechanisms in all their various forms."))
   echo result
   assert(result == "8df227ae87aee5cad77c395eb4a589469421f4d23ced1a8e93270cd4c4fd9cbf", "Incorrect hash")
 
-  result = %hmac_md5("Jefe", "what do ya want for nothing?")
+  result = toHex(hmac_md5("Jefe", "what do ya want for nothing?"))
   echo result
   assert(result == "750c783e6ab0b503eaa86e310a5db738","Incorrect hash")
 
-  result = %hmac_md5(longKey, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut nibh sit amet felis volutpat pellentesque eu at tellus. Etiam posuere justo eget mi porta porta.")
+  result = toHex(hmac_md5(longKey, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut nibh sit amet felis volutpat pellentesque eu at tellus. Etiam posuere justo eget mi porta porta."))
   echo result
   assert(result == "35acf8ac84d15ed02a4cd94331fc0aaa","Incorrect hash")
